390132e185562ac8a2e0c86eef2ad0733ae7f3e4
Timson <nskvolleytim@gmail.com>
2018-03-16 14:57:59 +0600

    Optimized gulp sass template

---
+++ app/js/common.js
@@ -0,0 +1,46 @@
+$(function() {
+
+	
+	//SVG Fallback
+	if(!Modernizr.svg) {
+		$("img[src*='svg']").attr("src", function() {
+			return $(this).attr("src").replace(".svg", ".png");
+		});
+	};
+
+
+	//E-mail Ajax Send
+	//Documentation & Example: https://github.com/agragregra/uniMail
+	$("form").submit(function() { //Change
+		var th = $(this);
+		$.ajax({
+			type: "POST",
+			url: "mail.php", //Change
+			data: th.serialize()
+		}).done(function() {
+			$(".form-callback .success ").addClass("active");
+			setTimeout(function() {
+				// Done Functions
+				$(".form-callback .success ").removeClass("active");
+				th.trigger("reset");
+				$.magnificPopup.close();
+			}, 3000);
+		});
+		return false;
+	});
+
+	//Chrome Smooth Scroll
+	try {
+		$.browserSelector();
+		if($("html").hasClass("chrome")) {
+			$.smoothScroll();
+		}
+	} catch(err) {
+
+	};
+
+	$("img, a").on("dragstart", function(event) { event.preventDefault(); });
+
+	
+
+});
